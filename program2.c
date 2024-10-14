fd30de9453ec56dd22110ea6b11c07e1069586cf
Saswata Mishra <cs24mtech12001@iith.ac.in>
2024-10-14 19:30:15 +0530

    adding GTK+ to attempt rendering bin tree

---
+++ program2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gtk/gtk.h>
+
+typedef struct tree_node
+{
+    long long int key;
+    struct tree_node * left;
+    struct tree_node * right;
+} node;
+
+
+int main() {
+
+
+}
